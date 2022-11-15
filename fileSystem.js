a8fd7bf2bbe1e346a59dd14fefcb601a6d295449
Aldrin Caballero <76159577+aldrin112602@users.noreply.github.com>
2022-11-15 11:34:55 +0800

    Update fileSystem.js

--- fileSystem.js
+++ fileSystem.js
@@ -1,69 +1,86 @@
-class FileSystem {
-  readFileAsDataURL(file) {
-    return new Promise((resolve, reject) => {
-      let reader = new FileReader()
-    if(file == ['object Blob'] || file == '[object File]') {
-      reader.readAsDataURL(file)
-    } else {
-      if(window.fetch) {
-       fetch(file).then(res => res.blob())
-       .then(res => reader.readAsDataURL(res))
-      } else {
-        let xhr = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-        xhr.open('GET', file, true)
-        xhr.responseType = 'blob'
-        xhr.onload = () => {
-          reader.readAsDataURL(xhr.response)
-        }
-        xhr.send(null)
-      }
-    }
-    reader.onload = e => {
-      resolve(e.target.result)
-    }
-    reader.onerror = () => {
-      reject('Failed to read file')
-    }
-    })
+"use strict";
+
+function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return !!right[Symbol.hasInstance](left); } else { return left instanceof right; } }
+function _classCallCheck(instance, Constructor) { if (!_instanceof(instance, Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+var FileSystem = function () {
+  function FileSystem() {
+    _classCallCheck(this, FileSystem);
   }
-  
-  readFile(file) {
-    return new Promise((resolve, reject) => {
-      if(window.fetch) {
-        window.fetch(file).then(res => res.text())
-        .then(res => {
-          resolve(res)
-        }).catch(err => {
-          reject(err)
-        })
-      } else {
-        let xhr = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP')
-        xhr.open('GET', file, true)
-        xhr.responseType = 'text'
-        xhr.onload = () => {
-          resolve(xhr.response)
+  _createClass(FileSystem, [{
+    key: "readFileAsDataURL",
+    value: function readFileAsDataURL(file) {
+      return new Promise(function (resolve, reject) {
+        var reader = new FileReader();
+        if (file == ['object Blob'] || file == '[object File]') {
+          reader.readAsDataURL(file);
+        } else {
+          if (window.fetch) {
+            fetch(file).then(function (res) {
+              return res.blob();
+            }).then(function (res) {
+              return reader.readAsDataURL(res);
+            });
+          } else {
+            var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+            xhr.open('GET', file, true);
+            xhr.responseType = 'blob';
+            xhr.onload = function () {
+              reader.readAsDataURL(xhr.response);
+            };
+            xhr.send(null);
+          }
         }
-        xhr.onerror = () => {
-          reject('Failed to read file')
+        reader.onload = function (e) {
+          resolve(e.target.result);
+        };
+        reader.onerror = function () {
+          reject('Failed to read file');
+        };
+      });
+    }
+  }, {
+    key: "readFile",
+    value: function readFile(file) {
+      return new Promise(function (resolve, reject) {
+        if (window.fetch) {
+          window.fetch(file).then(function (res) {
+            return res.text();
+          }).then(function (res) {
+            resolve(res);
+          }).catch(function (err) {
+            reject(err);
+          });
+        } else {
+          var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+          xhr.open('GET', file, true);
+          xhr.responseType = 'text';
+          xhr.onload = function () {
+            resolve(xhr.response);
+          };
+          xhr.onerror = function () {
+            reject('Failed to read file');
+          };
+          xhr.send(null);
         }
-        xhr.send(null)
-      }
-    })
-  }
-  
-  createFile(content, type, filename) {
-    var blob = new Blob([content], {
-      type: type
-    })
-    let URL_ = (window.URL || window.webkitURL);
-    let fileURL = URL_.createObjectURL(blob);
-    let a = document.createElement('a');
-    a.setAttribute('href', fileURL);
-    a.setAttribute('download', filename);
-    a.click();
-    a.remove();
+      });
+    }
+  }, {
+    key: "createFile",
+    value: function createFile(content, type, filename) {
+      var blob = new Blob([content], {
+        type: type
+      });
+      var URL_ = window.URL || window.webkitURL;
+      var fileURL = URL_.createObjectURL(blob);
+      var a = document.createElement('a');
+      a.setAttribute('href', fileURL);
+      a.setAttribute('download', filename);
+      a.click();
+      a.remove();
       URL_.revokeObjectURL(fileURL);
-  }
-  
-  
-}
+    }
+  }]);
+  return FileSystem;
+}();
